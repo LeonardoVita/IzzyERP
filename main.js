1e3f7fd457c0aed004a1fcc09837566299f9caa8
Leonardo Vita <Leonardo.vita0606@gmail.com>
2019-09-13 20:04:01 -0300

    iniciando projeto no github

---
+++ main.js
@@ -0,0 +1,30 @@
+const out = document.querySelector('#sai-home');
+const on = document.querySelector('#volta-home');
+const on2 = document.querySelector('#volta-home-logo'); 
+const on3 = document.querySelector('#volta-home-portrait');
+
+out.addEventListener('click', sai);
+on.addEventListener('click', entra);
+on2.addEventListener('click', entra);
+on3.addEventListener('click', entra);
+
+function sai() {
+    const frente = document.querySelector('.IzzyERP-Home');
+    frente.classList.add('out')
+}
+
+function entra() {
+    const frente = document.querySelector('.IzzyERP-Home');
+    frente.classList.remove('out')
+}
+
+
+function showGallery(moduleType){
+    const gallery = document.querySelector(moduleType);
+    gallery.classList.add('show')           
+}
+
+function exitGallery(moduleType){
+    const gallery = document.querySelector(moduleType);
+    gallery.classList.remove('show') 
+}
