84cb87ed99660cfa8e5b43ff8ca1c29083aa975a
leonardo.vita <leonaro.vita0606@gmail.com>
2019-09-13 22:24:04 -0300

    no coments

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh',`${vh}px`);
+
+window.addEventListener('resize', () => {
+// We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 const out = document.querySelector('#sai-home');
 const on = document.querySelector('#volta-home');
 const on2 = document.querySelector('#volta-home-logo'); 
