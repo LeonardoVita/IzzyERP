3f12cd8112298270db51a67919fe61bae35af376
leonardo.vita <leonaro.vita0606@gmail.com>
2019-09-13 22:38:15 -0300

    no coments

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-
+   
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh',`${vh}px`);
 
 window.addEventListener('resize', () => {
-// We execute the same script as before
+
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    console.log("resized")
 });
 
 const out = document.querySelector('#sai-home');
