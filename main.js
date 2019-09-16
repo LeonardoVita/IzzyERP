d509b555a390b8b5883388cb2c346208115951e0
leonardo.vita <leonaro.vita0606@gmail.com>
2019-09-16 02:19:39 -0300

    touch for carousel

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-   
-// let vh = screen.width * 0.01;
+
+// let vh = screen.height * 0.01;
 // document.documentElement.style.setProperty('--vh',`${vh}px`);
 
 // window.addEventListener('resize', () => {
 
-//     let vh = screen.width * 0.01;
+//     let vh = screen.height * 0.01;
 //     document.documentElement.style.setProperty('--vh', `${vh}px`);
-    
+
 // });
 
 const out = document.querySelector('#sai-home');
 const on = document.querySelector('#volta-home');
-const on2 = document.querySelector('#volta-home-logo'); 
+const on2 = document.querySelector('#volta-home-logo');
 const on3 = document.querySelector('#volta-home-portrait');
 
 out.addEventListener('click', sai);
@@ -30,12 +30,28 @@ function entra() {
 }
 
 
-function showGallery(moduleType){
+function showGallery(moduleType) {
     const gallery = document.querySelector(moduleType);
-    gallery.classList.add('show')           
+    gallery.classList.add('show')
 }
 
-function exitGallery(moduleType){
+function exitGallery(moduleType) {
     const gallery = document.querySelector(moduleType);
-    gallery.classList.remove('show') 
+    gallery.classList.remove('show')
 }
+
+$(".carousel").on("touchstart", function (event) {
+    var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function (event) {
+        var xMove = event.originalEvent.touches[0].pageX;
+        if (Math.floor(xClick - xMove) > 5) {
+            $(this).carousel('next');
+        }
+        else if (Math.floor(xClick - xMove) < -5) {
+            $(this).carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function () {
+        $(this).off("touchmove");
+    });
+});
